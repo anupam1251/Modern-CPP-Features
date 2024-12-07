ccaf60590bdc3a381fb0ba824004bbf9341104d5
Anupam Chaudhary <ak47.anupam125@gmail.com>
2024-12-08 02:29:25 +0530

    added functions in templates

--- Templates/main.cpp
+++ Templates/main.cpp
@@ -15,6 +15,17 @@ void foo(T1 input1, T2 input2) {
 	std::cout << typeid(input2).name() << std::endl;
 }
 
+template<typename T>
+bool equal(T a, T b) {
+	return (a == b);
+}
+
+template<>
+bool equal<float>(float a, float b) {
+	std::cout << "equal<float>" << std::endl;
+	return fabs(a-b) < 0.0001f;
+}
+
 int main() {
 	std::cout << square(5) << std::endl;
 	std::cout << square(5.5) << std::endl;
@@ -24,6 +35,12 @@ int main() {
 	std::cout << square2(7) << std::endl;
 
 	foo<int, float>(10, 3.5);
+
+	std::cout << equal<int>(2, 2) << std::endl;
+	std::cout << equal<int>(2, 3) << std::endl;
+
+	std::cout << equal<float>(1.0f- 0.999999f, 0.000001f) << std::endl;
+	std::cout << equal<float>(2.5f,3.5f) << std::endl;
 
 
 	return 0;
