e2b30a5eb7a27831a5cc746c1e02bb1a69e149fd
Anupam Chaudhary <ak47.anupam125@gmail.com>
2024-12-09 01:48:08 +0530

    completed assignment-1 umar lone

--- Templates/main.cpp
+++ Templates/main.cpp
@@ -26,6 +26,40 @@ bool equal<float>(float a, float b) {
 	return fabs(a-b) < 0.0001f;
 }
 
+template<typename T1, typename T2>
+auto Max(T1 x, T2 y) -> typename std::common_type<T1, T2>::type{
+	return x > y ? x : y;
+}
+
+template<typename T>
+T ArraySum(const T* pArr, size_t arrSize) {
+	T ans = 0;
+	for (int i = 0; i < arrSize; i++) {
+		ans += *(pArr + i);
+	}
+	return ans;
+}
+
+template<typename T>
+T Max(const T* pArr, size_t arrSize) {
+
+	T ans = INT_MIN;
+	for (int i = 0; i < arrSize; i++) {
+		ans = std::max(ans, *(pArr + i));
+	}
+	return ans;
+}
+
+template<typename T>
+std::pair<const T&, const T&> MinMax(const T* pArr, size_t arrSize) {
+	T s_elem = INT_MAX, l_elem = INT_MIN;
+	for (int i = 0; i < arrSize; i++) {
+		s_elem = std::min(s_elem, *(pArr + i));
+		l_elem = std::max(l_elem, *(pArr + i));
+	}
+	return std::make_pair(s_elem, l_elem);
+}
+
 int main() {
 	std::cout << square(5) << std::endl;
 	std::cout << square(5.5) << std::endl;
@@ -42,6 +76,22 @@ int main() {
 	std::cout << equal<float>(1.0f- 0.999999f, 0.000001f) << std::endl;
 	std::cout << equal<float>(2.5f,3.5f) << std::endl;
 
+	std::cout << Max(10, 20) <<"\n";
+	std::cout << Max(10.2f, 20.4f) <<"\n";
+	std::cout << Max(10, 20.4) << "\n";
+
+	int arr1[5] = { 1,2,3,4,15 };
+	std::cout << "ArraySum : " << ArraySum(arr1, 5) << "\n";
+
+	double arr2[5] = { 1.14,2.1,3.3,4.2,5.2 };
+	std::cout << "ArraySum : " << ArraySum(arr2, 5) << "\n";
+
+	std::cout << "arr1 Max : " << Max(arr1, 5) << "\n";
+	std::cout << "arr2 Max : " << Max(arr2, 5) << "\n";
+
+
+	std::cout << "MinMax of arr1 : " << MinMax(arr1, 5).first<<" " <<MinMax(arr1,5).second << "\n";
+	std::cout << "MinMax of arr2 : " << MinMax(arr2, 5).first << " " << MinMax(arr2, 5).second << "\n";
 
 	return 0;
 }
